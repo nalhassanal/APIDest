2aeaa82e54f289906d83e318b242e66b0cb45a82
Clement Maxwell <24608767+clementmaxwell@users.noreply.github.com>
2025-08-03 11:57:22 +0800

    Calling a basket of world bank API, return aggregated data

--- Business/Business/HomeBL.cs
+++ Business/Business/HomeBL.cs
@@ -1,7 +1,14 @@
-﻿namespace Business
+﻿using Entities;
+using Entities.Tariff;
+
+namespace Business
 {
     public class HomeBL
     {
+        // Preprocess fetched data
+        public void preprocessData(ViewModelTariffEscalationVsTradeDependence vm)
+        {
 
+        }
     }
 }
---
+++ Shared/Entities/ViewModelRawWorldBankData.cs
@@ -0,0 +1,26 @@
+﻿namespace Entities
+{
+    public class RawWorldBankData
+    {
+        public Indicator Indicator { get; set; }
+        public Country Country { get; set; }
+        public string CountryIso3Code { get; set; }
+        public string Date { get; set; }
+        public double? Value { get; set; }
+        public string Unit { get; set; }
+        public string ObsStatus { get; set; }
+        public int Decimal { get; set; }
+    }
+
+    public class Indicator
+    {
+        public string Id { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class Country
+    {
+        public string Id { get; set; }
+        public string Value { get; set; }
+    }
+}
